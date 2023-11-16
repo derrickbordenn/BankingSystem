b2ba14f4c715a31c661702772db1e03a76f08d15
Derrick <derr>
2023-11-16 01:15:37 -0500

    Add to process function to pass section creation test case (checking account)

--- src/main/java/CommandProcessor.java
+++ src/main/java/CommandProcessor.java
@@ -9,10 +9,10 @@ public class CommandProcessor {
 		String[] commandParts = command.split(" ");
 		String commandType = commandParts[0].toLowerCase();
 		String accountType = commandParts[1].toLowerCase();
-		int id = Integer.parseInt(commandParts[2]);
-		double apr = Double.parseDouble(commandParts[3]);
-		if (commandParts.length < 5) {
-			if (commandType.equals("create")) {
+		if (commandType.equals("create")) {
+			int id = Integer.parseInt(commandParts[2]);
+			double apr = Double.parseDouble(commandParts[3]);
+			if (commandParts.length < 5) {
 				if (accountType.equals("savings")) {
 					bank.addAccount(new SavingsAccount(id, apr));
 				}
