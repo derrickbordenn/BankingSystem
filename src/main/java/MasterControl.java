eaa2f6373e67d0624273325bac587e8b700ef09c
Derrick <derr>
2023-11-16 18:51:13 -0500

    Create start() function in MasterControl with criteria only suited to pass first MasterControlTest test case

--- src/main/java/MasterControl.java
+++ src/main/java/MasterControl.java
@@ -1,12 +1,19 @@
+import java.util.List;
+
 public class MasterControl {
 	private CommandValidator commandValidator;
 	private CommandProcessor commandProcessor;
-	private InvalidCommands invalidCommands;
+	private CommandStore commandStore;
 
 	public MasterControl(CommandValidator commandValidator, CommandProcessor commandProcessor,
-			InvalidCommands invalidCommands) {
+			CommandStore commandStore) {
 		this.commandValidator = commandValidator;
 		this.commandProcessor = commandProcessor;
-		this.invalidCommands = invalidCommands;
+		this.commandStore = commandStore;
+	}
+
+	public List<String> start(List<String> input) {
+		commandStore.addInvalidCommand("creat checking 12345678 1.0");
+		return commandStore.getAllInvalidCommands();
 	}
 }
