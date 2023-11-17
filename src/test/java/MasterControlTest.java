5f76fb0470b4299f5629fdde3c01599cc85aeb59
Derrick <derr>
2023-11-16 19:18:54 -0500

    Create test for invalid creation of two accounts with the same id

--- src/test/java/MasterControlTest.java
+++ src/test/java/MasterControlTest.java
@@ -52,4 +52,14 @@ public class MasterControlTest {
 		assertEquals("creat checking 12345678 1.0", actual.get(0));
 		assertEquals("depositt 12345678 100", actual.get(1));
 	}
+
+	@Test
+	void invalid_to_create_accounts_with_same_ID() {
+		input.add("create checking 12345678 1.0");
+		input.add("create checking 12345678 1.0");
+
+		List<String> actual = masterControl.start(input);
+
+		assertSingleCommand("create checking 12345768 1.0", actual);
+	}
 }
