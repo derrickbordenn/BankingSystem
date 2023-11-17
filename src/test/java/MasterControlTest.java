a68e987b915fd1a9358c6debbe5e06d37e3235f0
Derrick <derr>
2023-11-16 19:10:47 -0500

    Refactor declaration of input and combine both assertions to a single assert statement

--- src/test/java/MasterControlTest.java
+++ src/test/java/MasterControlTest.java
@@ -9,30 +9,35 @@ import org.junit.jupiter.api.Test;
 public class MasterControlTest {
 
 	MasterControl masterControl;
+	List<String> input;
 
 	@BeforeEach
 	void setUp() {
+		input = new ArrayList<>();
 		Bank bank = new Bank();
 		masterControl = new MasterControl(new CommandValidator(), new CommandProcessor(bank), new CommandStore());
 	}
 
+	private void assertSingleCommand(String command, List<String> actual) {
+		assertEquals(1, actual.size());
+		assertEquals(command, actual.get(0));
+	}
+
 	@Test
 	void typo_in_create_command_is_invalid() {
-		List<String> input = new ArrayList<>();
 		input.add("creat checking 12345678 1.0");
 
 		List<String> actual = masterControl.start(input);
-		assertEquals(1, actual.size());
-		assertEquals("creat checking 12345678 1.0", actual.get(0));
+
+		assertSingleCommand("creat checking 12345678 1.0", actual);
 	}
 
 	@Test
 	void typo_in_deposit_command_is_invalid() {
-		List<String> input = new ArrayList<>();
 		input.add("depositt 12345678 100");
 
 		List<String> actual = masterControl.start(input);
-		assertEquals(1, actual.size());
-		assertEquals("depositt 12345678 100", actual.get(0));
+
+		assertSingleCommand("depositt 12345678 100", actual);
 	}
 }
