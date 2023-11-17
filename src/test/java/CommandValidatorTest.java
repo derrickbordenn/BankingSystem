110fdfeb3bbf67f13340ccca82766fbc80f2d51e
Derrick <derr>
2023-11-17 02:31:00 -0500

    created instance of class with a Bank parameter so duplicate ids can be checked

--- src/test/java/CommandValidatorTest.java
+++ src/test/java/CommandValidatorTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 
 public class CommandValidatorTest {
 	CommandValidator commandValidator;
+	Bank bank;
 
 	@BeforeEach
 	public void setUP() {
-		commandValidator = new CommandValidator();
+		Bank bank = new Bank();
+		commandValidator = new CommandValidator(bank);
 	}
 
 	@Test
