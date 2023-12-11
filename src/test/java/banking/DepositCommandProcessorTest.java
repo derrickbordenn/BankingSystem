34f49e7d5a87c86bd39079f7f8ebc1255e4e5a6b
Derrick <derr>
2023-12-10 23:58:29 -0500

    add edge cases for deposit

--- src/test/java/banking/DepositCommandProcessorTest.java
+++ src/test/java/banking/DepositCommandProcessorTest.java
@@ -16,6 +16,24 @@ public class DepositCommandProcessorTest {
 	}
 
 	@Test
+	void deposit_zero_to_checking() {
+		bank.addAccount(new CheckingAccount(12345678, 2.4));
+		commandProcessor.process("deposit 12345678 0");
+		double actual = bank.getAccountById(12345678).getBalance();
+
+		assertEquals(0, actual);
+	}
+
+	@Test
+	void deposit_zero_to_savings() {
+		bank.addAccount(new SavingsAccount(12345678, 2.4));
+		commandProcessor.process("deposit 12345678 0");
+		double actual = bank.getAccountById(12345678).getBalance();
+
+		assertEquals(0, actual);
+	}
+
+	@Test
 	void deposit_to_empty_checking_makes_balance_deposit_amount() {
 		bank.addAccount(new CheckingAccount(12345678, 2.4));
 		commandProcessor.process("deposit 12345678 200");
@@ -51,5 +69,23 @@ public class DepositCommandProcessorTest {
 		double actual = bank.getAccountById(12345678).getBalance();
 
 		assertEquals(227, actual);
+	}
+
+	@Test
+	void deposit_maximum_to_checking() {
+		bank.addAccount(new CheckingAccount(12345678, 2.4));
+		commandProcessor.process("deposit 12345678 1000");
+		double actual = bank.getAccountById(12345678).getBalance();
+
+		assertEquals(1000, actual);
+	}
+
+	@Test
+	void deposit_maximum_to_savings() {
+		bank.addAccount(new SavingsAccount(12345678, 2.4));
+		commandProcessor.process("deposit 12345678 2500");
+		double actual = bank.getAccountById(12345678).getBalance();
+
+		assertEquals(2500, actual);
 	}
 }
