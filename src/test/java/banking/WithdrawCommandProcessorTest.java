823a59f42b37d7ac73b2fcb6499b16ce162e2598
Derrick <derr>
2023-12-11 00:11:43 -0500

    pushing to test pipeline

--- src/test/java/banking/WithdrawCommandProcessorTest.java
+++ src/test/java/banking/WithdrawCommandProcessorTest.java
@@ -16,7 +16,7 @@ public class WithdrawCommandProcessorTest {
 	void setUp() {
 		bank = new Bank();
 		commandProcessor = new CommandProcessor(bank);
-		savings = new SavingsAccount(1234578, 2.4);
+		savings = new SavingsAccount(12345678, 2.4);
 		checking = new CheckingAccount(12345679, 2.4);
 		cd = new CDAccount(12345678, 2.1, 2000);
 	}
@@ -24,28 +24,10 @@ public class WithdrawCommandProcessorTest {
 	@Test
 	public void withdrawing_from_savings_subtracts_amount_from_balance() {
 		bank.addAccount(savings);
-		savings.deposit_money(250);
-		command
-		assertEquals();
-	}
-
-	@Test
-	public void withdrawing_money_twice_from_same_account_subtracts_withdrawal_amount_twice_from_balance() {
-		savings.deposit_money(DEPOSIT_AMOUNT);
-		savings.withdraw_money(WITHDRAWAL_AMOUNT);
-		savings.withdraw_money(WITHDRAWAL_AMOUNT);
-
+		bank.depositById(12345678, 250);
+		commandProcessor.process("withdraw 12345678 150");
 		double actual = savings.getBalance();
 
-		assertEquals(DEPOSIT_AMOUNT - WITHDRAWAL_AMOUNT * 2, actual);
-	}
-
-	@Test
-	public void withdrawing_larger_amount_than_account_balance_does_not_result_in_negative_balance() {
-		savings.deposit_money(DEPOSIT_AMOUNT);
-		savings.withdraw_money(DEPOSIT_AMOUNT + 1);
-		double actual = savings.getBalance();
-
-		assertEquals(0, actual);
+		assertEquals(100, actual);
 	}
 }
